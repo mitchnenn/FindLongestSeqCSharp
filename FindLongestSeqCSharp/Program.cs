e0d7034ec629986b8e4289e55a1582a70c5f3e14
Mitch Nenn <mitch.nenn@earthlink.net>
2021-03-27 09:08:51 -0700

    Used FParsec to parse the file in C#. Works in 8 ms.

--- FindLongestSeqCSharp/Program.cs
+++ FindLongestSeqCSharp/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FParsec.CSharp;
+using static FParsec.CSharp.PrimitivesCS; // combinator functions
+using static FParsec.CSharp.CharParsersCS; // pre-defined parsers
 
 namespace FindLongestSeqCSharp
 {
     partial class Program
     {
+        private static IEnumerable<int> ParseInput()
+        {
+            var countParser = Int.AndL(Newline);
+            var seqParser = Many(Spaces.AndRTry(Int));
+            var entryParser = Tuple(countParser, seqParser);
+            var allEntries = Many1(entryParser);
+            var path = Path.Combine(AppContext.BaseDirectory, "data.txt");
+            var result = allEntries.RunOnFile(path).GetResult();
+            return result[0].Item2;
+        }
+        
         static void Main(string[] args)
         {
-            var input = new [] {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+            var input = ParseInput().ToArray();
             var state = new LongestSeqAlgorithm(input);
             var stopWatch = Stopwatch.StartNew();
             var longestIncSeq = state.FindLongestIncreasingSeq();
